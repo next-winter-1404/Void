7e1d18a15103b490997acee166a54d8a1fa2e85f
armin <knightarmin@gmail.com>
2026-06-03 19:51:54 +0330

    dashlayout

--- src/app/(Dashboard)/layout.tsx
+++ src/app/(Dashboard)/layout.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Dashsidebar from '@/components/DashboardComps/Dashsidebar'
+import DashHeader from '@/components/DashboardComps/DashHeader'
+export default function DashLayout({children}:Readonly<{children:React.ReactNode}>){
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+    return(
+        <div className='flex bg-zinc-200 gap-10 h-screen' dir='rtl'>
+          <Dashsidebar />
+          <main dir="rtl" className="flex-1 flex flex-col p-6 overflow-y-auto">
+            <DashHeader />
+          {children}
+          </main>
+        </div>
+    )
+
 }
-
-export default layout
---
+++ src/components/DashboardComps/DashHeader.tsx
@@ -0,0 +1,59 @@
+'use client' 
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+// import { Bell } from 'lucide-react';
+import arrowdash from '@/assets/Images/Dashboard/arrowdash.png'
+import moon from '@/assets/Images/Dashboard/moon.png'
+import Sun from '@/assets/Images/Dashboard/Sun.png'
+import Bell2 from '@/assets/Images/Dashboard/Bell2.png'
+
+const pageTitles: { [key: string]: string } = {
+  '/dashboard': 'داشبورد',
+  '/profile': 'اطلاعات کاربری',
+  '/properties': 'مدیریت املاک',
+  '/reservations': 'مدیریت رزرو ها',
+};
+
+export default function DashHeader() {
+  const pathname = usePathname();
+  const title = pageTitles[pathname] || 'داشبورد'; 
+
+  return (
+    <header className="flex items-center justify-between p-4 bg-white border border-gray-200 rounded-2xl shadow-sm w-full ">
+      
+      
+      <div className="flex items-center gap-2 font-bold text-lg">
+         <span>{title}</span>
+         <span className="text-gray-400"><Image src={arrowdash} alt='>>>'></Image></span>
+      </div>
+
+      
+      <div className="flex items-center gap-4" dir='ltr'>
+        
+        
+        <div className="flex items-center gap-3">
+          <div className="text-right">
+            <p className="text-sm font-semibold">سبحان عرب خزائلی</p>
+            <p className="text-xs text-gray-400">فروشنده</p>
+          </div>
+          <div className="w-10 h-10 bg-gray-200 rounded-lg"></div>
+        </div>
+
+        
+        <div className="h-8 w-px bg-gray-300"></div>
+
+        
+        <button className="p-2 text-gray-600">
+           <Image src={Bell2} alt='D'></Image>
+        </button>
+
+        {/* سوییچ تم (ساده‌سازی شده) */}
+        <div className="flex items-center bg-gray-100 p-1 rounded-full w-20 justify-between">
+           <span className="bg-green-400 p-1 rounded-full text-white"><Image src={Sun} alt='W'></Image></span>
+           <span className="p-1"><Image src={moon} alt='m'></Image></span>
+        </div>
+
+      </div>
+    </header>
+  );
+}
