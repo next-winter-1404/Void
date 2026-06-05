fc8ccd536c8e7398874459eb4979fe0e47023bcc
Mersad <mosaybimersvd020@gmail.com>
2026-06-05 10:22:04 +0330

    bug fix

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,9 +4,9 @@ import DashHeader from '@/components/DashboardComps/DashHeader'
 export default function DashLayout({children}:Readonly<{children:React.ReactNode}>){
 
     return(
-        <div className='flex bg-zinc-200 gap-10 min-h-screen ' dir='rtl'>
+        <div className='flex bg-zinc-200 gap-10 min-h-screen' dir='rtl'>
           <Dashsidebar />
-          <main dir="rtl" className=" flex-1 flex flex-col p-6 overflow-y-auto">
+          <main dir="rtl" className="flex-1 flex flex-col p-6 overflow-y-auto">
             <DashHeader />
           {children}
           </main>
